8ff68358d9a08d26d61a745725a3dfe7fec14b4a
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-05-27 15:30:28 +0200

    Adding last commits

--- live.php
+++ live.php
@@ -2,10 +2,18 @@
 <section>
     <?php foreach($lastCommits as $lastCommit) : ?>
     <div class="w-100">
-        <?= $lastCommit->getAuthor()->getName(); ?>
-        <?= $lastCommit->getDate()->format('Y-m-d'); ?>
-        <?= $lastCommit->getMessage(); ?>
-        <?= $lastCommit->getFiles(); ?>
+        <table>
+            <tbody>
+            <tr><th>ID<td><?= $lastCommit->getId(); ?>
+            <tr><th>Contributor<td><?= $lastCommit->getAuthor()->getName(); ?>
+            <tr><th>Date<td><?= $lastCommit->getDate()->format('Y-m-d h:i:s'); ?>
+            <tr><th>Message<td><?= $lastCommit->getMessage(); ?>
+            <tr><th>Files<td><ul>
+                <?php $files = $lastCommit->getFiles(); foreach($files as $path => $file) : ?>
+                    <li><?= $path; ?>
+                <?php endforeach; ?>
+                </ul>
+        </table>
     </div>
     <?php endforeach; ?>
 </section>
