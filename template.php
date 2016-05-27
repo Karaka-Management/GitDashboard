f9b9922b1a9dc11bc398d56d9d00c3a8caf06359
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-05-27 13:19:31 +0200

    Auto reloader added

--- template.php
+++ template.php
@@ -1,14 +1,11 @@
-<?php
-if(!defined('ROOT_PATH')) {
-    return;
-}
-?>
+<?php if(!defined('ROOT_PATH')) return; ?>
 <!DOCTYPE HTML>
 <html>
 <head>
     <title>GitDashboard</title>
     <link rel="stylesheet" href="styles.css" type="text/css" />
     <link rel="stylesheet" href="../cssOMS/chart/chart_line.css" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" type="text/css" />
     <script src="http://d3js.org/d3.v3.min.js"></script>
     <script src="../jsOMS/Utils/oLib.js"></script>
     <script src="../jsOMS/Chart/Chart.js"></script>
@@ -24,3 +21,19 @@ if(!defined('ROOT_PATH')) {
     <?php include 'repository.php'; ?>
     <?php include 'live.php'; ?>
 </main>
+<script>
+    jsOMS.ready(function() {
+        jsOMS.watcher(function() {
+            let xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function() {
+                if(xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                    document.write(xmlHttp.responseText);
+                    document.close();
+                }
+            };
+
+            xmlHttp.open('GET', location.href, true);
+            xmlHttp.send();
+        }, <?= $CONFIG['update_interval'] * 1000; ?>);
+    });
+</script>
